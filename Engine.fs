4e0b07c2ea2981f45f6aeb873d466f5a4467d026
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2024-02-08 00:50:51 +0700

    extracted winforms specific code to a separate file

---
+++ Engine.fs
@@ -0,0 +1,40 @@
+﻿module Engine
+
+open System.Windows.Forms
+open System.Drawing
+
+
+type Color = Green | Red
+type Shape = Rect of x: int * y: int * w: int * h: int * color: Color
+type Game<'state> = { title: string; state: 'state; update: 'state -> 'state * Shape seq }
+
+
+let private dotnetColor = function
+    | Green -> System.Drawing.Color.Green
+    | Red   -> System.Drawing.Color.Red
+
+
+type MyForm<'state>(state: 'state, update) as this =
+    inherit Form()
+
+    let mutable state = state
+    let mutable shapes = Seq.empty
+    let timer = new Timer(Interval = 100)
+    do
+        timer.Tick.Add <| fun _ ->
+            let (a, b) = update state
+            state <- a
+            shapes <- b
+            this.Refresh()
+        timer.Start()
+
+    override _.OnPaint e =
+        base.OnPaint(e)
+        for sh in shapes do
+            match sh with
+            | Rect(x, y, w, h, c) -> e.Graphics.FillRectangle(new SolidBrush(dotnetColor c), x, y, w, h)
+
+
+let run<'state> game =
+    Application.Run(new MyForm<'state>(game.state, game.update, Width = 800, Height = 600, Text = game.title))
+
--- Program.fs
+++ Program.fs
@@ -1,27 +1,15 @@
-﻿open System.Windows.Forms
-open System.Drawing
+﻿module Program
+
+open Engine
 
 
-let mutable x = 0
+type State = { x: int }
 
+let update { x = x } = 
+    let shapes = seq { Rect(x, 10, 10, 10, Red) 
+                       Rect(x, 20, 10, 10, Green) }
+    ({ x = x + 10 }, shapes)
 
-type MyForm =
-    inherit Form
+run { title = "Snake"; state = { x = 0 }; update = update }
 
-    new() as this =
-        let timer = new Timer(Interval = 100)
-        timer.Tick.Add <| fun _ ->
-            x <- x + 10
-            this.Refresh()
-        timer.Start()
-        {}
-
-    override _.OnPaint e =
-        base.OnPaint(e)
-        e.Graphics.FillRectangle(new SolidBrush(Color.Green), x, 100, 200, 150)
-
-
-let f = new MyForm(Width = 800, Height = 600, Text = "Hello")
-//f.Controls.Add(new Label(Text = "Hello World!", Left = 100))
-Application.Run(f)
 
