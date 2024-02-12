d0546d2fcb591c8ccb47967c7108bd473724945a
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2024-02-13 02:46:06 +0700

    eating apples

--- src/Game.fs
+++ src/Game.fs
@@ -10,16 +10,24 @@ type Dir = Up | Down | Left | Right
 type Snake = { length: int; cells: CellXY list; dir: Dir; }
 type State = { field: Field; snake: Snake; apples: CellXY list }
 
+type CellState = Empty | Apple
+
 
 let keyToDir = function | Key.Up -> Up | Key.Down -> Down | Key.Left -> Left | Key.Right -> Right
 let move (x, y) = function | Up -> (x, y - 1) | Down -> (x, y + 1) | Left -> (x - 1, y) | Right -> (x + 1, y)
 
+let cellState state xy = if List.contains xy state.apples then Apple else Empty
+
 
 let update (state: State) input = 
-    let dir = match Seq.tryLast input with | Some(key) -> keyToDir key | None -> state.snake.dir
-    let snake = { state.snake with cells = List.truncate state.snake.length (move state.snake.cells[0] dir :: state.snake.cells)
-                                   dir = dir }
-    { state with snake = snake }
+    let newDir = match Seq.tryLast input with | Some(key) -> keyToDir key | None -> state.snake.dir
+    let newHeadXY = move state.snake.cells[0] newDir
+    let newLength = state.snake.length + match cellState state newHeadXY with | Empty -> 0 | Apple -> 5
+
+    { state with snake = { length = newLength
+                           cells = List.truncate newLength (newHeadXY :: state.snake.cells)
+                           dir = newDir }
+                 apples = List.except [ newHeadXY ] state.apples }
 
 
 let draw cellSize state =
@@ -37,9 +45,9 @@ let randomApple _ = (rand field.width, rand field.height)
 run { title = "Snake"
       size = (800, 600)
       init = { field = field
-               snake = { length = 25; cells = [ (5, 5) ]; dir = Right }
+               snake = { length = 5; cells = [ (5, 5) ]; dir = Right }
                apples = List.init 5 randomApple }
       update = update
       draw = draw 10
-      fps = 20 }
+      fps = 10 }
 
